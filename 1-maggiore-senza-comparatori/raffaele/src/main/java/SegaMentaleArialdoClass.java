26e6c6782354fcb72576142cc50b7cd314b876ee
Arialdo Martini <arialdomartini@gmail.com>
2017-09-21 15:41:26 +0200

    Raffaele's version refactored to be very short

--- 1-maggiore-senza-comparatori/raffaele/src/main/java/SegaMentaleArialdoClass.java
+++ 1-maggiore-senza-comparatori/raffaele/src/main/java/SegaMentaleArialdoClass.java
@@ -3,9 +3,8 @@ public class SegaMentaleArialdoClass {
      * Yes, That's Java, folks!
      */
     public int comparator(int a, int b) {
-        return (int)
-                (a * (Math.signum(a - b) + 1) / 2 +
-                        b * (Math.signum(b - a) + 1) / 2 -
-                        a * (Math.signum(a - b) + Math.signum(b - a)) / 2);
+
+        return (int) (a + b + Math.signum(a - b) * (a - b)) /2;
     }
+
 }
