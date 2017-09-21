1dc86843e76eb5cedc3c4889ec4aa9d21cd9d028
Arialdo Martini <arialdomartini@gmail.com>
2017-09-21 10:21:46 +0200

    Seconda soluzione di Raffaele

--- 1-maggiore-senza-comparatori/raffaele/src/main/java/SegaMentaleArialdoClass.java
+++ 1-maggiore-senza-comparatori/raffaele/src/main/java/SegaMentaleArialdoClass.java
@@ -3,6 +3,9 @@ public class SegaMentaleArialdoClass {
      * Yes, That's Java, folks!
      */
     public int comparator(int a, int b) {
-        return (int) ((a / 2) * (Math.signum(a - b) + 1) + b * Math.signum(b - a));
+        return (int)
+                (a * (Math.signum(a - b) + 1) / 2 +
+                        b * (Math.signum(b - a) + 1) / 2 -
+                        a * (Math.signum(a - b) + Math.signum(b - a)) / 2);
     }
 }
