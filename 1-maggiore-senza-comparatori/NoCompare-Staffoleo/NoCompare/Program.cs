f77472fec6f9fa2a16600e475448cf7b1f766519
Staffolani Leonardo <leonardo.staffolani@tcpos.com>
2017-09-21 09:18:38 +0200

    added unit test

--- 1-maggiore-senza-comparatori/NoCompare-Staffoleo/NoCompare/Program.cs
+++ 1-maggiore-senza-comparatori/NoCompare-Staffoleo/NoCompare/Program.cs
@@ -15,25 +15,12 @@ namespace NoCompare
       Console.WriteLine("Insert second number:");
       var number2 = Convert.ToInt32(Console.ReadLine());
 
-      var result = Max(number1, number2);
+      var result = new Comparer().Max(number1, number2);
       Console.WriteLine($"The bigger number is: {result}");
 
       Console.ReadLine();
     }
 
-    private static int Max(int number1, int number2)
-    {
-      try
-      {
-        var result = number1 - number2;
-        Convert.ToUInt32(result);
-
-        return number1;
-      }
-      catch
-      {
-        return number2;
-      }
-    }
+    
   }
 }
