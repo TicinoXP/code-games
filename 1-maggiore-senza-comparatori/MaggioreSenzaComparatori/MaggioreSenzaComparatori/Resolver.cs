1d0ca854ed847a4b218a9f34073991e0c979b4ca
Arialdo Martini <arialdomartini@gmail.com>
2017-09-24 07:35:05 +0200

    Max comparator has been implemented

---
+++ 1-maggiore-senza-comparatori/MaggioreSenzaComparatori/MaggioreSenzaComparatori/Resolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaggioreSenzaComparatori
+{
+    public class Resolver
+    {
+        public int Max(int a, int b)
+        {
+            var media = ((double)a + b) / 2;
+            var diff = Math.Abs((double)a - b) / 2;
+            
+            return (int)(media + diff);
+        }
+    }
+}
---
+++ 1-maggiore-senza-comparatori/MaggioreSenzaComparatori/MaggioreSenzaComparatoriTest/ResolverTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using MaggioreSenzaComparatori;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaggioreSenzaComparatoriTest
+{
+    public class ResolverTest
+    {
+        Resolver _sut = new Resolver();
+
+        [TestCase(0, 0, 0)]
+        [TestCase(10, 0, 10)]
+        [TestCase(0, 10, 10)]
+        [TestCase(-1, 0, 0)]
+        [TestCase(-1, 1, 1)]
+        [TestCase(-10, -1, -1)]
+        [TestCase(9, 10, 10)]
+        [TestCase(10, 9, 10)]
+        [TestCase(-10, -9, -9)]
+        [TestCase(-9, -10, -9)]        
+        public void Max_should_work_correctly(int a, int b, int expected)
+        {
+            _sut.Max(a, b).Should().Be(expected);
+        }
+    }
+}
